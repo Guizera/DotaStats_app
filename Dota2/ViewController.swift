12bc7c75b79db4f2a9c30b4698fd5b79df765069
Guilherme Cunha <guizera7@gmail.com>
2021-05-21 16:57:09 -0300

    Initial Commit

---
+++ Dota2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dota2
+//
+//  Created by José Alves da Cunha on 21/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
